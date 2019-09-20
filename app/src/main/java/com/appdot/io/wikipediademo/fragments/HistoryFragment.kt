94aa06abfcce776e084e23f6ad63be08a459773e
iamAbayomi <Oladinifolawiyo@gmail.com>
2019-09-20 18:50:38 +0100

    Cached History Results

--- app/src/main/java/com/appdot/io/wikipediademo/fragments/HistoryFragment.kt
+++ app/src/main/java/com/appdot/io/wikipediademo/fragments/HistoryFragment.kt
@@ -1,22 +1,24 @@
 package com.appdot.io.wikipediademo.fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 
 import com.appdot.io.wikipediademo.R
+import com.appdot.io.wikipediademo.WikiApplication
 import com.appdot.io.wikipediademo.adapters.ArticleCardRecyclerAdapter
+import com.appdot.io.wikipediademo.adapters.ArticleListItemRecyclerAdapter
+import com.appdot.io.wikipediademo.managers.WikiManager
+import com.appdot.io.wikipediademo.model.WikiPage
 import kotlinx.android.synthetic.main.fragment_history.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 
 /**
  * A simple [Fragment] subclass.
@@ -24,22 +26,68 @@ private const val ARG_PARAM2 = "param2"
  */
 class HistoryFragment : Fragment() {
 
+    private var wikiManager: WikiManager? = null
     var historyRecycler: RecyclerView? = null
+    private val adapter = ArticleListItemRecyclerAdapter()
+
+    init {
+        setHasOptionsMenu(true)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        wikiManager = (activity?.applicationContext as WikiApplication).wikiManager
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-                                     ): View? {
+    ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_history, container, false)
 
         historyRecycler = view.findViewById<RecyclerView>(R.id.history_article_recycler)
 
         historyRecycler!!.layoutManager = LinearLayoutManager(context)
-        historyRecycler!!.adapter = ArticleCardRecyclerAdapter()
-
+        historyRecycler!!.adapter = adapter
 
         return view
     }
 
+    override fun onResume() {
+        super.onResume()
+        doAsync {
+            val history = wikiManager!!.getHistory()
+            adapter.currentResults.clear()
+            adapter.currentResults.addAll(history as ArrayList<WikiPage>)
+            activity?.runOnUiThread { adapter.notifyDataSetChanged() }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater!!.inflate(R.menu.history_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == R.id.action_clear_history) {
+            //show confirmation alert
+            activity?.alert ("Are you sure want to clear your history?", "Confirm")
+            { yesButton {
+                // yes was hit...
+                // clear history async
+                    adapter.currentResults.clear()
+                    doAsync{
+                        wikiManager?.clearHistory()
+                    }
+                }
+                noButton {
+
+                }
+            }?.show()
+        }
+        return true
+    }
 
 }
