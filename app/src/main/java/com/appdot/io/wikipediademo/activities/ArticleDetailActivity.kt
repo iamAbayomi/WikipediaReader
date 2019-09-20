325b800ca52e28e1907bb925423fd1098325ed04
iamAbayomi <Oladinifolawiyo@gmail.com>
2019-09-20 16:06:47 +0100

    Added WikiMangerInstance

--- app/src/main/java/com/appdot/io/wikipediademo/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/appdot/io/wikipediademo/activities/ArticleDetailActivity.kt
@@ -2,37 +2,60 @@ package com.appdot.io.wikipediademo.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.appdot.io.wikipediademo.R
+import com.appdot.io.wikipediademo.WikiApplication
+import com.appdot.io.wikipediademo.managers.WikiManager
 import com.appdot.io.wikipediademo.model.WikiPage
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_article_detail.*
 
 class ArticleDetailActivity : AppCompatActivity(){
 
+    private var wikiManager: WikiManager? = null
     private var currentPage: WikiPage? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
 
+        wikiManager =(applicationContext as WikiApplication).wikiManager
+
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
 
         //get the page from the extras
         val wikiPageJson = intent.getStringExtra("page")
         currentPage = Gson().fromJson<WikiPage>(wikiPageJson, WikiPage::class.java)
 
-        article_detail_webView?.webViewClient = object : WebViewClient() {
+       supportActionBar?.title = currentPage?.title
+
+
+        article_detail_webView?.webViewClient = object: WebViewClient(){
+
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 return true
             }
-
         }
+
+        article_detail_webView.loadUrl(currentPage!!.fullurl)
+        wikiManager?.addHistory(currentPage!!)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.article_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if(item!!.itemId== android.R.id.home){
             finish()
