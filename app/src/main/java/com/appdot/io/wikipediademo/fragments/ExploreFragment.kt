174ab0a6ccd790b7c3d5fa9ce77eb0445744707c
iamAbayomi <Oladinifolawiyo@gmail.com>
2019-09-20 17:26:04 +0100

    Cached Search Results in ExploreFragment

--- app/src/main/java/com/appdot/io/wikipediademo/fragments/ExploreFragment.kt
+++ app/src/main/java/com/appdot/io/wikipediademo/fragments/ExploreFragment.kt
@@ -1,6 +1,7 @@
 package com.appdot.io.wikipediademo.fragments
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -9,32 +10,36 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 
 import com.appdot.io.wikipediademo.R
+import com.appdot.io.wikipediademo.WikiApplication
 import com.appdot.io.wikipediademo.activities.SearchActivity
 import com.appdot.io.wikipediademo.adapters.ArticleCardRecyclerAdapter
+import com.appdot.io.wikipediademo.managers.WikiManager
 import com.appdot.io.wikipediademo.provider.ArticleDataProvider
 import kotlinx.android.synthetic.main.fragment_explore.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  *
  */
 class ExploreFragment : Fragment() {
-    private val articleProvider : ArticleDataProvider = ArticleDataProvider()
+    private var wikiManager: WikiManager? = null
     var searchCardView: CardView? = null
     var exploreRecycler : RecyclerView? = null
     var adapter : ArticleCardRecyclerAdapter = ArticleCardRecyclerAdapter()
     var refresher:  SwipeRefreshLayout? =null
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        wikiManager = (activity?.applicationContext as WikiApplication).wikiManager
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -49,11 +54,10 @@ class ExploreFragment : Fragment() {
             context!!.startActivity(searchIntent)
         }
 
-        exploreRecycler!!.layoutManager = LinearLayoutManager(context)
+        exploreRecycler!!.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         exploreRecycler!!.adapter = adapter
 
         refresher?.setOnRefreshListener {
-
             getRandomArticles()
         }
         getRandomArticles()
@@ -63,9 +67,8 @@ class ExploreFragment : Fragment() {
 
     private fun getRandomArticles(){
         refresher?.isRefreshing= true
-
         try {
-            articleProvider.getRandom(15, { wikiResult ->
+            wikiManager?.getRandom(15, { wikiResult ->
                 adapter.currentResults.clear()
                 adapter.currentResults.addAll(wikiResult.query!!.pages)
                 activity?.runOnUiThread {
